bbd6689ecfdb8efbe12419f68b957adf3003c096
mohd-hassan17 <ansarimohdhassan3@gmail.com>
2025-11-14 21:59:08 +0530

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,25 @@
+import { HeroSection } from "@/components/landing/hero";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+  <>
+  <main className="bg-white dark:bg-black/5 overflow-x-hidden">
+            <div className="grid grid-rows-[auto_1fr_auto] min-h-screen px-6 lg:px-4 gap-4 sm:gap-12">
+                <HeroSection />
+            </div>
+
+            {/* <ComponentShowcaseCard
+            className="mx-auto w-full max-w-7xl px-4 sm:px-6 py-12 md:py-16 lg:py-20"
+            /> */}
+            {/* <InteractivePreview/> */}
+            {/* <BuildInterfacesCard */}
+            {/* className="mx-auto w-full max-w-7xl px-4 sm:px-6 py-12 md:py-16 lg:py-20" */}
+            {/* /> */}
+          
+            {/* <TechnologyBadges/> */}
+            {/* <EventToast /> */}
+        </main>
+  </> 
+  );
+}
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,36 @@
-import type { NextConfig } from "next";
+import { createMDX } from "fumadocs-mdx/next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const withMDX = createMDX();
+
+const nextConfig = {
+    pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+    outputFileTracingIncludes: {
+        "/**": ["components/codesnippetui/**/*"],
+    },
+    async headers() {
+        return [
+            {
+                source: "/r/:path*",
+                headers: [
+                    {
+                        key: "Cache-Control",
+                        value: "public, max-age=31536000, immutable",
+                    },
+                ],
+            },
+        ];
+    },
+    images: {
+        remotePatterns: [
+            {
+                hostname: "*",
+            },
+        ],
+    },
+    reactStrictMode: true,
+    eslint: {
+        ignoreDuringBuilds: true,
+      },
 };
 
-export default nextConfig;
+export default withMDX(nextConfig);
